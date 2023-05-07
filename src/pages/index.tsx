7454bd12496784cfc2647341e70836e8913f98f9
John Beadles <jbeardles@gmail.com>
2023-05-06 23:08:37 -0500

    Reorg some things

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import Navbar from "~/components/Navbar";
+import { PageLayout } from "~/components/layout";
+
+import Navbar from "~/components/ui/Navbar";
 import { PokemonList } from "~/components/ui/PokmeonList";
 
 
@@ -12,12 +14,7 @@ const Home: NextPage = () => {
         <meta name="description" content="Make your own Pokedex!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="min-h-screen bg-gradient-to-b from-blue-500 via-blue-900 to-yellow-500">
-        <Navbar />
-        <div className="container mx-auto">
-          <PokemonList />
-        </div>
-      </main>
+      <PageLayout></PageLayout>
     </>
   );
 };
